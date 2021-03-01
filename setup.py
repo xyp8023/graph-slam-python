4d186eeb95ef6b4dd9e666e6aaf90cc171f93883
xyp8023 <xyp8023@gmail.com>
2021-03-01 13:15:10 +0100

    fix bugs

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='graph-slam-python', version='1.0', python_requires='3.6.7', packages=find_packages())
+setup(name='graph-slam-python', version='1.0', python_requires='>=3.6.7', packages=find_packages())
