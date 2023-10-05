4cc3a15b9254beb5792de77aed123acf498c27f4
Yongkie Wiyogo <6004558-ywiyogo@users.noreply.gitlab.com>
2023-10-05 22:07:57 +0200

    Fix error and deprecated functions

--- utils/read_data.py
+++ utils/read_data.py
@@ -3,14 +3,14 @@ This script is to parse the raw data and extract useful information for later.
 The datasets used in this slam demo are from http://ais.informatik.uni-freiburg.de/slamevaluation/datasets.php
 """
 
-import os
+import os, sys
 import numpy as np
 from pathlib import Path
 
+
 def read_data(name):
     root_path = os.getcwd()
-    
-    
+
     clf_file = root_path + os.sep + "datasets" + os.sep + name + ".clf"
     # print(clf_file)
     # check if the dataset is existed
@@ -31,40 +31,47 @@ def read_data(name):
     logger_ts = []
 
     # parse the dataset
-    with open(clf_file, "r") as f: 
+    with open(clf_file, "r") as f:
         for line in f:
-            
             if line.startswith("FLASER"):
                 read_line = line.split()
 
                 # number of range readings, should be 180
                 num_readings = int(read_line[1])
-                assert num_readings==180
-                range_readings = np.array(read_line[2:2+num_readings], dtype=np.float)
-                index = np.arange(-90, 90+180/num_readings, 180/num_readings)
-                index = np.delete(index, num_readings//2)
-                angles = np.radians(index)
-                
-                converted_readings = np.array([np.cos(angles), np.sin(angles)])*range_readings
-                lasers.append(converted_readings)
-                
-                x = read_line[2+num_readings]
-                y = read_line[3+num_readings]
-                theta = read_line[4+num_readings]
-                odoms_laser.append([ float(x), float(y), float(theta)])
 
-                ipc_timestamp = read_line[8+num_readings]
-                logger_timestamp = read_line[10+num_readings]
+                range_readings = np.array(read_line[2 : 2 + num_readings], dtype=float)
+                if num_readings == 180:
+                    index = np.arange(-90, 91)
+                elif num_readings == 360:
+                    index = np.arange(-180, 181)
+                else:
+                    raise ValueError("Laser reading {} invalid", num_readings)
+                index = np.delete(index, num_readings // 2)
+                angles = np.radians(index)
+
+                converted_readings = (
+                    np.array([np.cos(angles), np.sin(angles)]) * range_readings
+                )
+                lasers.append(converted_readings)
+
+                x = read_line[2 + num_readings]
+                y = read_line[3 + num_readings]
+                theta = read_line[4 + num_readings]
+                odoms_laser.append([float(x), float(y), float(theta)])
+
+                ipc_timestamp = read_line[8 + num_readings]
+                logger_timestamp = read_line[10 + num_readings]
                 ipc_ts.append(ipc_timestamp)
                 logger_ts.append(logger_timestamp)
-
 
     odoms_laser = np.array(odoms_laser)
     lasers = np.array((lasers))
     ipc_ts = np.array(ipc_ts)
     logger_ts = np.array(logger_ts)
 
-    odoms_laser_file = root_path + os.sep + "datasets" + os.sep + name + "_odoms_laser.npy" 
+    odoms_laser_file = (
+        root_path + os.sep + "datasets" + os.sep + name + "_odoms_laser.npy"
+    )
     if not os.path.exists(odoms_laser_file):
         np.save(odoms_laser_file, odoms_laser)
 
@@ -72,14 +79,17 @@ def read_data(name):
     if not os.path.exists(lasers_file):
         np.save(lasers_file, lasers)
 
-    ipc_ts_file = root_path + os.sep + "datasets" + os.sep + name + "_ipc_ts.npy" 
+    ipc_ts_file = root_path + os.sep + "datasets" + os.sep + name + "_ipc_ts.npy"
     if not os.path.exists(ipc_ts_file):
         np.save(ipc_ts_file, ipc_ts)
 
-    logger_ts_file = root_path + os.sep + "datasets" + os.sep + name + "_logger_ts.npy" 
+    logger_ts_file = root_path + os.sep + "datasets" + os.sep + name + "_logger_ts.npy"
     if not os.path.exists(logger_ts_file):
         np.save(logger_ts_file, logger_ts)
 
+
 if __name__ == "__main__":
     name = "aces"
+    if sys.argv[1] != "":
+        name = sys.argv[1]
     read_data(name=name)
